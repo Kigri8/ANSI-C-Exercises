8c87a92822a4350709d0213442fe322e522f86a8
Kigri8 <78566395+Kigri8@users.noreply.github.com>
2021-04-15 07:57:40 -0500

    getop.c

--- homework4.1.c
+++ homework4.1.c
@@ -5,11 +5,9 @@
 int getop(char s[])
 {
     int i,c;
-
     while((s[0] = c = getch()) == ' ' || c =='\t')
         ;
     s[1] = '\0';
-
     i = 0;
     if(!isdigit(c) && c!='.' && c!='-'){
         if (!isalpha(c))
@@ -17,27 +15,22 @@ int getop(char s[])
         if (isalpha(c)){
         return tolower(c);
         }
-
     }
-
     if(c=='-')
         if(isdigit(c=getch()) || c == '.')
             s[++i]=c;
         else
         {
-            if(c!=EOF)
-                ungetch(c);
-            return '-';
+        if(c!=EOF)
+        ungetch(c);
+        return '-';
         }
-
     if(isdigit(c))
         while(isdigit(s[++i] =c =getch()))
             ;
-
     if(c=='.')
         while(isdigit(s[++i] = c=getch()))
             ;
-
     s[i] = '\0';
     if(c!=EOF)
         ungetch(c);
