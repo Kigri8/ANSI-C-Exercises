d470049a4e613ebf24cf098b731cd227e28c4640
Kigri8 <78566395+Kigri8@users.noreply.github.com>
2021-04-22 09:43:42 -0500

    .

---
+++ homework5.h
@@ -0,0 +1,26 @@
+#include <stdio.h> // HOMEWORK5_By Kigri
+#include <stdlib.h>
+#include <time.h>
+void initialize(int, char **);
+void free_space(void);
+void show(int **);
+int obtainNumber(int, int);
+void rellenar();
+void showDiff();
+int state(int , int);
+#define NUMBER '1'
+void push(int);
+void pushlet(char);
+int pop(void);
+char poplet(void);
+int getop(char []);
+int getch(void);
+void ungetch(int);
+#define NUMBER '1'
+int getop (char[]);
+int getch (void);
+void ungetch (int);
+void showcompletematrix(int** , int );
+void showmatrixwhileplay(int** , int , int** );
+int count(int** , int , int , int );
+int find(int** , int** , int , int , int );
