07c648ac0d6c1a876189f67f661d19d0eefea3f9
Kigri8 <78566395+Kigri8@users.noreply.github.com>
2021-05-06 05:20:22 -0500

    Create salida.c

---
+++ Homework7/salida.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define PE 6
+int main(int x, char** y) {
+  int k, i = 0;
+  char username[UNAME + 1]; 
+  char pr[PE + 1];
+  char number[ID + 1];
+  FILE* entrada = fopen("datos.txt", "r");
+  FILE* salida = fopen("reorden.txt", "w");
+  while (fscanf(entrada, "%s %s %s\n", number, pr, username) == 3) {
+#ifdef CONVERT
+    fprintf(salida, "Student %d %s (%d) is %s\n", ++i, username, atoi(number), pr);
+#else
+    fprintf(salida, "Student %d %s (%s) is %s\n", ++i, username, number, pr);
+#endif
+#ifdef DEBUG
+    printf(".");
+#endif
+  }
+#ifdef DEBUG
+    printf("\n");
+#endif    
+  fclose(entrada);
+  fclose(salida);
+  return 0;
+}
