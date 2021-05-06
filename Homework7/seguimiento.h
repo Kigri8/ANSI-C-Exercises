8a10a3aa3bbf175dab3cba41c0aa07302d1045ee
Kigri8 <78566395+Kigri8@users.noreply.github.com>
2021-05-06 05:20:43 -0500

    Create seguimiento.h

---
+++ Homework7/seguimiento.h
@@ -0,0 +1,10 @@
+#define MAX 20
+#define USUARIOS 26
+int* registrados = 0;
+int** matrix;
+int totaldesalidas=0;
+int count();
+void registrar();
+void entradaosalida(int, int);
+void header();
+void horario();
